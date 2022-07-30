fc17d36adf436f348be44f7db83eeb377f26fe08
Cancey Dejean <canceyd@gmail.com>
2022-07-30 14:08:18 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      body: ["stratos-lights", "sans-serif"],
+      title: ["filson-pro", "sans-serif"],
+      button: ["stratos", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
